37f86b8715f6bff91023af70fe68fe545593c3e4
Eric Stokes <fernferret@gmail.com>
2011-02-11 12:18:04 -0500

    Add GridView code

--- GridView/src/com/fernferret/hellogridview/HelloGridView.java
+++ GridView/src/com/fernferret/hellogridview/HelloGridView.java
@@ -2,12 +2,16 @@ package com.fernferret.hellogridview;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.GridView;
 
 public class HelloGridView extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		
+		GridView gridview = (GridView) findViewById(R.id.gridview);
+		gridview.setAdapter(new ImageAdapter(this));
+	}
 }
