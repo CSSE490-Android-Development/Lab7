9d8a2ed2350e5d17bec89e23d47860322594ae34
Jimmy Theis <jimmy@jetheis.com>
2011-02-11 00:52:01 -0500

    Finish Spinner lab
    Signed-off-by: Jimmy Theis <jimmy@jetheis.com>

---
+++ Spinner/src/com/jetheis/spinner/SpinnerMain.java
@@ -0,0 +1,21 @@
+package com.jetheis.spinner;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class SpinnerMain extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.planets_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new MyOnItemSelectedListener());
+    }
+}
